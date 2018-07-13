e517722120fbfaf6ef905918a8913fb9d1d4db60
Kudo Shinichi <hongyi.shen00@rafflesgirlssch.edu.sg>
2018-07-14 07:55:07 +0800

    > test SignUpVC

---
+++ See GO/Controllers/SignUpViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SignUpViewController.swift
+//  See GO
+//
+//  Created by Hongyi Shen on 14/7/18.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
