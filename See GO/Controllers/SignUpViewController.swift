904453b77173c53fdd522aa10fadc607d1da3c2f
Kudo Shinichi <hongyi.shen00@rafflesgirlssch.edu.sg>
2018-07-14 08:37:47 +0800

    > guards against invalid emails and PW

--- See GO/Controllers/SignUpViewController.swift
+++ See GO/Controllers/SignUpViewController.swift
@@ -42,9 +42,26 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     // MARK: Actions
     @IBAction func createAccount(_ sender: Any) {
         
+        //check if email exists alr
+        
         guard emailText.text != "", passwordText.text != "", usernameText.text != "" else {
-            // if some fields are incomplete, UIAlertView pops out to alert
             let alert = UIAlertController(title: "Missing fields", message: "No email, password, or username. Check again?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+            
+            return
+        }
+        
+        guard isValidEmail(testStr: emailText.text!) else {
+            let alert = UIAlertController(title: "Invalid Email", message: "Try again?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+            
+            return
+        }
+        
+        guard isValidPassword(testStr: passwordText.text!) else {
+            let alert = UIAlertController(title: "Invalid Password", message: "Please create passwords with at least 8 characters, one letter and one number.", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: nil))
             self.present(alert, animated: true)
             
@@ -70,6 +87,23 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
         group.notify(queue: .main){
             self.performSegue(withIdentifier: "SignUpToMap", sender: nil)
         }
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    
+    func isValidPassword(testStr:String?) -> Bool {
+        guard testStr != nil else { return false }
+        
+        // at least one digit
+        // at least one lowercase
+        // 8 characters total
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "(?=.*[0-9])(?=.*[a-z]).{8,}")
+        return passwordTest.evaluate(with: testStr)
     }
     
     override func didReceiveMemoryWarning() {
