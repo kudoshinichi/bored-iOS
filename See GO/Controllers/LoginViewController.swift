fb9af2a83af72bf5fb71c48df082a2585a6fd70d
Kudo Shinichi <hongyi.shen00@rafflesgirlssch.edu.sg>
2018-07-14 08:05:33 +0800

    > Connected Login and Sign Up View Controllers (SignUpVC works)

--- See GO/Controllers/LoginViewController.swift
+++ See GO/Controllers/LoginViewController.swift
@@ -2,33 +2,78 @@
 //  LoginViewController.swift
 //  See GO
 //
-//  Created by Hongyi Shen on 14/7/18.
+//  Created by Hongyi Shen on 13/7/18.
 //
 
 import UIKit
+import Firebase
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            // ...
+        }
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        emailText.delegate = self
+        passwordText.delegate = self
+        
     }
-
+    
+    //MARK: Actions
+    @IBAction func LoginAccount(_ sender: Any) {
+        
+        let email = emailText.text
+        let password = passwordText.text
+        
+        print(email)
+        print(password)
+        
+        guard emailText.text != "", passwordText.text != "" else {
+            // if some fields are incomplete, UIAlertView pops out to alert
+            let alert = UIAlertController(title: "Missing fields", message: "No email, password, or username. Check again?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+            
+            return
+        }
+        
+        
+        let group = DispatchGroup()
+        group.enter()
+        DispatchQueue.main.async {
+            Auth.auth().signIn(withEmail: email!, password: password!) { (user, error) in
+                // ...
+            }
+            group.leave()
+        }
+        
+        group.notify(queue: .main){
+            self.performSegue(withIdentifier: "LoginToMap", sender: nil)
+        }
+        
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- See GO/Controllers/SignUpViewController.swift
+++ See GO/Controllers/SignUpViewController.swift
@@ -2,33 +2,80 @@
 //  SignUpViewController.swift
 //  See GO
 //
-//  Created by Hongyi Shen on 14/7/18.
+//  Created by Hongyi Shen on 8/7/18.
 //
 
 import UIKit
+import Firebase
 
-class SignUpViewController: UIViewController {
+var handle: AuthStateDidChangeListenerHandle?
 
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            // ...
+        }
+        
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        usernameText.delegate = self
+        emailText.delegate = self
+        passwordText.delegate = self
+        
     }
-
+    
+    // MARK: Actions
+    @IBAction func createAccount(_ sender: Any) {
+        
+        guard emailText.text != "", passwordText.text != "", usernameText.text != "" else {
+            // if some fields are incomplete, UIAlertView pops out to alert
+            let alert = UIAlertController(title: "Missing fields", message: "No email, password, or username. Check again?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+            
+            return
+        }
+        
+        let emailTextD = self.emailText.text!
+        let passwordTextD = self.passwordText.text
+        
+        let group = DispatchGroup()
+        group.enter()
+        DispatchQueue.main.async {
+            Auth.auth().createUser(withEmail: emailTextD, password: passwordTextD!) { (authResult, error) in
+                // ...
+            }
+            
+            Auth.auth().signIn(withEmail: emailTextD, password: passwordTextD!) { (user, error) in
+                // ...
+            }
+            group.leave()
+        }
+        
+        group.notify(queue: .main){
+            self.performSegue(withIdentifier: "SignUpToMap", sender: nil)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
