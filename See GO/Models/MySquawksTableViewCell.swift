a1017e9798ea533ed324a0769ea76035571d4ca8
Kudo Shinichi <hongyi.shen00@rafflesgirlssch.edu.sg>
2018-07-15 18:56:18 +0800

    > Linked (empty) View Controllers and Views to respective files

---
+++ See GO/Models/MySquawksTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MySquawksTableViewCell.swift
+//  See GO
+//
+//  Created by Hongyi Shen on 15/7/18.
+//
+
+import UIKit
+
+class MySquawksTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
