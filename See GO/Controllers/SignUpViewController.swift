96a7b6a50ea83256bab14b8c8d9543f03d6526a7
Kudo Shinichi <hongyi.shen00@rafflesgirlssch.edu.sg>
2018-08-11 11:54:23 +0800

    > removed useless @IBAction func & link (which caused "unrecognized selector sent to instance" crash if i delete it) > Google Sign In still works :)

--- See GO/Controllers/SignUpViewController.swift
+++ See GO/Controllers/SignUpViewController.swift
@@ -161,10 +161,6 @@ class SignUpViewController: UIViewController, UITextFieldDelegate, GIDSignInUIDe
          
     }
     
-    @IBAction func googleSignUp(_ sender: Any) {
-        
-    }
-    
     func isValidEmail(testStr:String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         
