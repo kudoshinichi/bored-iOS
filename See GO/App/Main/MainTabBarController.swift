b8c67246013b7484b0c43f689a98fdfad8d2441b
Bhargav Singapuri <bhsingapuri@gmail.com>
2019-03-28 20:30:14 +0800

    brought the TabController to the front, other changes to layout and positioning of view controllers. standardising naming conventions

---
+++ See GO/App/Main/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  See GO
+//
+//  Created by Bhargav Singapuri on 28/3/19.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
